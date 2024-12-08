3aa720360b1fb00dab8da848fc5bd60e9ac44364
Jari Jokinen <jari@j2digital.io>
2024-12-08 04:45:34 +0200

    Add twsapi_handshake()

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "twsapi.h"
 
@@ -6,8 +7,14 @@
 
 int main(void)
 {
+  printf("Connecting to TWS API...\n");
   int sockfd = twsapi_connect(TWSAPI_IPADDR, TWSAPI_PORT);
+  if (sockfd) {
+    printf("Connected to TWS API successfully\n");
+  }
+
   twsapi_disconnect(sockfd);
+  printf("Disconnected from TWS API\n");
 
   exit(EXIT_SUCCESS);
 }
--- src/twsapi.c
+++ src/twsapi.c
@@ -1,8 +1,42 @@
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include "twsapi.h"
+
+int twsapi_handshake(int sockfd, int min_client_ver, int max_client_ver)
+{
+  if (send(sockfd, "API", 4, 0) < 0) {
+    perror("send() error");
+    return -1;
+  }
+
+  char client_ver[10];
+  snprintf(client_ver, sizeof(client_ver), "v%d..%d", min_client_ver, 
+    max_client_ver);
+  twsapi_send(sockfd, client_ver);
+
+  return 0;
+}
+
+void twsapi_pack(const char *str, uint8_t **packed_msg, size_t *packed_msg_len)
+{
+  size_t str_len = strlen(str);
+
+  *packed_msg_len = sizeof(uint32_t) + str_len;
+  *packed_msg = malloc(*packed_msg_len);
+  if (*packed_msg == NULL) {
+    perror("malloc() error");
+    exit(EXIT_FAILURE);
+  }
+
+  uint32_t str_len_n = htonl(str_len);
+  memcpy(*packed_msg, &str_len_n, sizeof(uint32_t));
+
+  memcpy(*packed_msg + sizeof(uint32_t), str, str_len);
+}
 
 int twsapi_connect(const char *ipaddr, int port)
 {
@@ -29,6 +63,12 @@ int twsapi_connect(const char *ipaddr, int port)
     return -1;
   }
 
+  if (twsapi_handshake(sockfd, 100, 157) < 0) {
+    perror("twsapi_handshake() error");
+    close(sockfd);
+    return -1;
+  }
+
   return sockfd;
 }
 
@@ -40,5 +80,15 @@ int twsapi_disconnect(int sockfd)
 
 int twsapi_send(int sockfd, const char *msg)
 {
+  size_t packed_msg_len = 0;
+  uint8_t *packed_msg = NULL;
+
+  twsapi_pack(msg, &packed_msg, &packed_msg_len);
+
+  if (send(sockfd, &packed_msg, packed_msg_len, 0) < 0) {
+    perror("send() error");
+    return -1;
+  }
+
   return 0;
 }
